e9ec1bad94839b8f47be8e62eed7dcdcd9c6fd21
anazhook <ana.zhook@gmail.com>
2024-01-10 14:50:13 +0300

    encryption/decryption

--- src/main/java/com/semester/app/App.java
+++ src/main/java/com/semester/app/App.java
@@ -19,34 +19,81 @@ public class App {
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(System.in));
 
-        // String inpath = reader.readLine();
-        // String crpath = reader.readLine();
-        // String decrpath = reader.readLine();
+        System.out.println("enter input and output files names:");
+        String inpath = reader.readLine();
+        String outpath = reader.readLine();
 
-        String inpath = "somefile.txt";
-        String crpath = "cr.txt";
-        String decrpath = "dcr.txt";
-
+        String format = "";
+        int i = inpath.lastIndexOf('.');
+        if (i > 0) {
+            format = inpath.substring(i + 1);
+        }
+        String temppath = "temp." + format;
         // Generate a new key pair
         KeyPair keyPair = ac.generateKeyPair();
 
-        // Read the input file into a byte array
-        byte[] inputBytes = Files.readAllBytes(Paths.get(inpath));
+        System.out.println("type of input file (1 - plain, 2 - encrypted, 3 - archived):");
+        int command = Integer.parseInt(reader.readLine());
 
-        // Encrypt the input bytes
-        byte[] encryptedBytes = ac.encrypt(inputBytes, keyPair.getPublic());
+        byte[] inputBytes;
 
-        // Write the encrypted bytes to the output file
-        Files.write(Paths.get(crpath), encryptedBytes);
+        if (command == 1) {
+            // Read the input file into a byte array
+            inputBytes = Files.readAllBytes(Paths.get(inpath));
+            Files.write(Paths.get(temppath), inputBytes);
 
-        // Read the encrypted file into a byte array
-        byte[] encryptedBytesFromFile = Files.readAllBytes(Paths.get(crpath));
+        }
 
-        // Decrypt the encrypted bytes
-        byte[] decryptedBytes = ac.decrypt(encryptedBytesFromFile, keyPair.getPrivate());
+        else if (command == 2) {
+            // Read the encrypted file into a byte array
+            byte[] encryptedBytesFromFile = Files.readAllBytes(Paths.get(inpath));
+            // Decrypt the encrypted bytes
+            byte[] decryptedBytes = ac.decrypt(encryptedBytesFromFile, keyPair.getPrivate());
+            // Write the decrypted bytes to the output file
+            Files.write(Paths.get(temppath), decryptedBytes);
+        }
 
-        // Write the decrypted bytes to the output file
-        Files.write(Paths.get(decrpath), decryptedBytes);
+        else if (command == 3) {
+
+        }
+
+        else {
+            System.out.println("wrong command!");
+        }
+
+        System.out.println("type of output file (1 - plain, 2 - encrypted, 3 - archived):");
+        command = Integer.parseInt(reader.readLine());
+        inputBytes = Files.readAllBytes(Paths.get(temppath));
+
+        if (command == 1) {
+            Files.write(Paths.get(outpath), inputBytes);
+        }
+
+        else if (command == 2) {
+            // Encrypt the input bytes
+            byte[] encryptedBytes = ac.encrypt(inputBytes, keyPair.getPublic());
+
+            // Write the encrypted bytes to the output file
+            Files.write(Paths.get(outpath), encryptedBytes);
+        }
+
+        else if (command == 3) {
+
+        }
+
+        else {
+            System.out.println("wrong command!");
+        }
+
+        // // Read the encrypted file into a byte array
+        // byte[] encryptedBytesFromFile = Files.readAllBytes(Paths.get(outpath));
+
+        // // Decrypt the encrypted bytes
+        // byte[] decryptedBytes = ac.decrypt(encryptedBytesFromFile,
+        // keyPair.getPrivate());
+
+        // // Write the decrypted bytes to the output file
+        // Files.write(Paths.get(temppath), decryptedBytes);
     }
 
     public String getMessage() {
