643991e78eb2d676966d1d18f0d3b141f6176091
Anastasiya <125749225+anazhook@users.noreply.github.com>
2024-01-15 17:03:13 +0300

    encryption and decryption by password works

--- src/main/java/com/semester/app/Encrypting.java
+++ src/main/java/com/semester/app/Encrypting.java
@@ -1,67 +1,59 @@
 package com.semester.app;
 
-import javax.crypto.Cipher;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.security.*;
-import java.security.spec.X509EncodedKeySpec;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
 
 public class Encrypting {
-    private static final String ALGORITHM = "RSA";
+    private static final String ALGORITHM = "AES";
+    private static final int KEY_SIZE = 128;
 
-    public KeyPair generateKeyPair() throws NoSuchAlgorithmException {
-        KeyPairGenerator keyGen = KeyPairGenerator.getInstance(ALGORITHM);
-        keyGen.initialize(2048);
-        return keyGen.generateKeyPair();
-    }
-
-    void encrypt(String inputFilePath, String encryptedFilePath, PublicKey key) throws Exception {
+    public static void encryptFile(String inputFile, String outputFile, String encryptionKey) throws Exception {
+        byte[] key = encryptionKey.getBytes();
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key, ALGORITHM);
         Cipher cipher = Cipher.getInstance(ALGORITHM);
-        cipher.init(Cipher.ENCRYPT_MODE, key);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
 
-        byte[] keyBytes = key.getEncoded();
-
-        try (FileInputStream fis = new FileInputStream(inputFilePath);
-                FileOutputStream fos = new FileOutputStream(encryptedFilePath)) {
-
-            // Write the key bytes to the beginning of the file
-            fos.write(keyBytes);
-
-            // Encrypt the file content and write it to the file
-            byte[] buffer = new byte[8192];
+        try (InputStream inputStream = new FileInputStream(inputFile);
+                OutputStream outputStream = Files.newOutputStream(Path.of(outputFile), StandardOpenOption.CREATE)) {
+            outputStream.write(key);
+            byte[] buffer = new byte[1024];
             int bytesRead;
-            while ((bytesRead = fis.read(buffer)) != -1) {
-                byte[] encryptedBytes = cipher.doFinal(buffer, 0, bytesRead);
-                fos.write(encryptedBytes);
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                byte[] encryptedBytes = cipher.update(buffer, 0, bytesRead);
+                outputStream.write(encryptedBytes);
             }
+            byte[] finalBytes = cipher.doFinal();
+            outputStream.write(finalBytes);
         }
     }
 
-    void decrypt(String encryptedFilePath, String decryptedFilePath, PrivateKey key) throws Exception {
+    public static void decryptFile(String inputFile, String outputFile, String encryptionKey) throws Exception {
+        byte[] key = encryptionKey.getBytes();
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key, ALGORITHM);
+
         Cipher cipher = Cipher.getInstance(ALGORITHM);
-        cipher.init(Cipher.DECRYPT_MODE, key);
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
 
-        try (FileInputStream fis = new FileInputStream(encryptedFilePath);
-                FileOutputStream fos = new FileOutputStream(decryptedFilePath)) {
-
-            // Read the key bytes from the beginning of the file
-            byte[] keyBytes = new byte[2048];
-            fis.read(keyBytes);
-
-            // Initialize the decryption cipher with the loaded key bytes
-            KeyFactory keyFactory = KeyFactory.getInstance(ALGORITHM);
-            PublicKey publicKey = keyFactory.generatePublic(new X509EncodedKeySpec(keyBytes));
-            cipher.init(Cipher.DECRYPT_MODE, publicKey);
-
-            // Decrypt the file content and write it to the file
-            byte[] buffer = new byte[8192];
-            int bytesRead;
-            while ((bytesRead = fis.read(buffer)) != -1) {
-                byte[] decryptedBytes = cipher.doFinal(buffer, 0, bytesRead);
-                fos.write(decryptedBytes);
+        try (InputStream inputStream = Files.newInputStream(Path.of(inputFile));
+                OutputStream outputStream = new FileOutputStream(outputFile)) {
+            byte[] fileKey = new byte[key.length];
+            inputStream.read(fileKey);
+            if (!encryptionKey.equals(new String(fileKey))) {
+                throw new IllegalArgumentException("Incorrect encryption key!");
             }
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                byte[] decryptedBytes = cipher.update(buffer, 0, bytesRead);
+                outputStream.write(decryptedBytes);
+            }
+            byte[] finalBytes = cipher.doFinal();
+            outputStream.write(finalBytes);
         }
     }
 }
